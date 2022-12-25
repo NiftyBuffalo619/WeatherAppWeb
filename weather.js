5b58e56240b042dc9543fa26d1cc9ba80561c142
NiftyBuffalo619 <polaclukes@outlook.cz>
2022-12-25 15:39:35 +0100

    Third Commit

---
+++ weather.js
@@ -0,0 +1,11 @@
+/*
+https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,
+precipitation,weathercode,windspeed_10m&daily=weathercode,
+temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,
+precipitation_sum&current_weather=true&timeformat=unixtime&timezone=Europe%2FBerlin
+*/
+
+
+export function getWeather(lat , lon , timezone) {
+
+}
